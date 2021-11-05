4a0d78ed2d751574a7160acc81089ca2ac853c20
Monica Cruz <mcatcruz@gmail.com>
2021-11-04 19:23:25 -1000

    Troubleshooting heroku

--- app/__init__.py
+++ app/__init__.py
@@ -14,13 +14,15 @@ def create_app(test_config=None):
     app = Flask(__name__)
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('SQLALCHEMY_TEST_DATABASE_URI')
+    
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
+
     # # setting a SQLAlchemy config
     # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # # establishing connection string to the database, 5432 is the port for postgres
