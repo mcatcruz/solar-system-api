b9d256d2f07805589bee79e04be96141db941194
Monica Cruz <mcatcruz@gmail.com>
2021-11-04 18:49:57 -1000

    Added .env

--- app/__init__.py
+++ app/__init__.py
@@ -14,20 +14,18 @@ def create_app(test_config=None):
     app = Flask(__name__)
     if not test_config:
         app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-            "SQLALCHEMY_DATABASE_URI")
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-            "SQLALCHEMY_TEST_DATABASE_URI")
-
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
-    # setting a SQLAlchemy config
-    #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # establishing connection string to the database, 5432 is the port for postgres
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    # # setting a SQLAlchemy config
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    # # establishing connection string to the database, 5432 is the port for postgres
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
     # connects db and migrate to Flask app, using package syntax
     db.init_app(app)
     migrate.init_app(app, db)
